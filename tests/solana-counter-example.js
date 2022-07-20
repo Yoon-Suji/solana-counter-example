9b7dbae05c4d7ecac7cc1b392135785ec5bcd6ae
Yoon-Suji <jidl88@ewhain.net>
2022-07-20 17:07:46 +0900

    feat: add test code

--- tests/solana-counter-example.js
+++ tests/solana-counter-example.js
@@ -1,13 +1,47 @@
 const anchor = require("@project-serum/anchor");
+const { assert } = require("chai");
 
 describe("solana-counter-example", () => {
   // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
   anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.SolanaCounterExample;
+  const counter = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.SolanaCounterExample;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize().accounts({
+      counterAccount: counter.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([counter]).rpc();
+
+    const counterData = await program.account.counter.fetch(counter.publicKey);
+    assert.exists(counterData.count);
+    console.log(counterData);
+    console.log("Your transaction signature ", tx);
   });
+
+  it("Increments the counter", async () => {
+    const NUMBER = "5";
+    await program.rpc.increment(new anchor.BN(NUMBER), {
+      accounts: {
+        counterAccount: counter.publicKey,
+      },
+    });
+    const counterData = await program.account.counter.fetch(counter.publicKey);
+    assert.ok(counterData.count.toString() === NUMBER);
+    console.log(counterData.count);
+  });
+
+  it("Resets the counter", async () => {
+    const NUMBER = "1";
+    await program.rpc.reset(new anchor.BN(NUMBER), {
+      accounts: {
+        counterAccount: counter.publicKey,
+      },
+    });
+    const counterData = await program.account.counter.fetch(counter.publicKey);
+    assert.ok(counterData.count.toString() === NUMBER);
+    console.log(counterData.count);
+  })
 });
