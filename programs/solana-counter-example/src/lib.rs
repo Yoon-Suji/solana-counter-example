f57dcd1c360e612dc935c1c339cc399f5d5beda5
Yoon-Suji <jidl88@ewhain.net>
2022-07-20 16:52:16 +0900

    feat: add increment function

--- programs/solana-counter-example/src/lib.rs
+++ programs/solana-counter-example/src/lib.rs
@@ -11,8 +11,23 @@ pub mod solana_counter_example {
         counter_account.count = 0;
         Ok(())
     }
+
+    pub fn increment(ctx: Context<Increment>, count: u64) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        let current_count = counter_account.count;
+
+        // overflow check
+        if u64::MAX - current_count >= count {
+            counter_account.count = current_count + count;
+            return Ok(());
+        }
+        
+        counter_account.count = u64::MAX;
+        return Ok(())
+    }
 }
 
+// Transaction instructions
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     pub system_program: Program <'info, System>,
@@ -24,6 +39,14 @@ pub struct Initialize<'info> {
     pub user: Signer<'info>
 }
 
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, Counter>
+}
+
+// An account that goes inside a transaction instruction
 #[account]
 pub struct Counter {
     pub count: u64
