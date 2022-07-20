34c531115a4627f70f2e5e72f99ec1f821c3a0af
Yoon-Suji <jidl88@ewhain.net>
2022-07-20 16:33:25 +0900

    feat: add initialize function

--- programs/solana-counter-example/src/lib.rs
+++ programs/solana-counter-example/src/lib.rs
@@ -7,9 +7,24 @@ pub mod solana_counter_example {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.count = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    pub system_program: Program <'info, System>,
+
+    #[account(init, payer = user, space = 16 + 16)]
+    pub counter_account: Account<'info, Counter>,
+
+    #[account(mut)]
+    pub user: Signer<'info>
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64
+}
